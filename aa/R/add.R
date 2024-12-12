df9e4451dd111a967e159bcf6d4fc2ad195c2e08
surenny <121717160+surenny@users.noreply.github.com>
2024-12-12 14:25:52 +0800

    Add files via upload

---
+++ aa/R/add.R
@@ -0,0 +1,13 @@
+#' Add two names
+#' @param x num1
+#' @param y num2
+#' @return return a number
+#' @examples
+#' add(1,0)
+#' add(1,2)
+#'
+
+add <- function(x,y) {
+  z = x + y
+  print(z)
+}
--- aa/R/new.R
+++ aa/R/new.R
@@ -1,12 +1,5 @@
-#' Add two names
-#' @param x num1
-#' @param y num2
-#' @return return a number
-#' @examples
-#' add(1,0)
-#' add(1,2)
-#'
 
-add <- function(x,y) {
-  z = x + y
+
+new <- function() {
+  print("Hello, world!")
 }
